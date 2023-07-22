5b6c3bd11af2b0c785c3842a82bf8803e35cfc95
Mykhailo Matviiv <firenero94@gmail.com>
2023-07-22 03:41:51 +0200

    Add Json serialization to TypeIdDecoded

---
+++ src/FastIDs.TypeId.Serialization/TypeId.Serialization.JsonNet/TypeIdDecodedConverter.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace FastIDs.TypeId.Serialization.JsonNet;
+
+public class TypeIdDecodedConverter : JsonConverter<TypeIdDecoded>
+{
+    public override void WriteJson(JsonWriter writer, TypeIdDecoded value, JsonSerializer serializer)
+    {
+        writer.WriteValue(value.ToString());
+    }
+
+    public override TypeIdDecoded ReadJson(JsonReader reader, Type objectType, TypeIdDecoded existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        return reader.Value is string val ? TypeId.Parse(val).Decode() : default;
+    }
+}
---
+++ src/FastIDs.TypeId.Serialization/TypeId.Serialization.SystemTextJson/TypeIdDecodedConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FastIDs.TypeId.Serialization.SystemTextJson;
+
+public class TypeIdDecodedConverter : JsonConverter<TypeIdDecoded>
+{
+    public override TypeIdDecoded Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var val = reader.GetString();
+        return val is not null ? TypeId.Parse(val).Decode() : default;
+    }
+
+    public override void Write(Utf8JsonWriter writer, TypeIdDecoded value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
