179a0a6f67b5437e4e3ffd30191e40bede471447
Mykhailo Matviiv <firenero94@gmail.com>
2024-05-12 04:58:53 +0200

    Implement formattable interfaces in TypeIdDecoded

--- src/FastIDs.TypeId/TypeId.Tests/TypeIdTests/FormattingTests.cs
+++ src/FastIDs.TypeId/TypeId.Tests/TypeIdTests/FormattingTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
+using System.Text.Unicode;
 using FastIDs.TypeId;
 using FluentAssertions;
 
@@ -62,10 +64,55 @@ public class FormattingTests
     }
     
     [TestCaseSource(typeof(TestCases), nameof(TestCases.ValidIds))]
+    public void Decoded_GetSuffixUtf8Span(string typeIdStr, Guid expectedGuid, string expectedType)
+    {
+        var decoded = TypeId.FromUuidV7(expectedType, expectedGuid);
+        
+        var expectedSuffix = typeIdStr.Split('_')[^1];
+    
+        Span<byte> utf8Suffix = stackalloc byte[expectedSuffix.Length + 10];
+        var bytesWritten = decoded.GetSuffix(utf8Suffix);
+        
+        bytesWritten.Should().Be(expectedSuffix.Length);
+        var suffix = Encoding.UTF8.GetString(utf8Suffix[..bytesWritten]);
+        suffix.Should().Be(expectedSuffix);
+    }
+    
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.ValidIds))]
     public void Decoded_ToString(string typeIdStr, Guid expectedGuid, string expectedType)
     {
         var decoded = TypeId.FromUuidV7(expectedType, expectedGuid);
 
         decoded.ToString().Should().Be(typeIdStr);
+    }
+
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.ValidIds))]
+    public void Decoded_ToStringFormat(string typeIdStr, Guid expectedGuid, string expectedType)
+    {
+        var decoded = TypeId.FromUuidV7(expectedType, expectedGuid);
+
+        decoded.ToString("", null).Should().Be(typeIdStr);
+    }
+    
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.ValidIds))]
+    public void Decoded_TryFormat(string typeIdStr, Guid expectedGuid, string expectedType)
+    {
+        var decoded = TypeId.FromUuidV7(expectedType, expectedGuid);
+
+        Span<char> formattedTypeId = stackalloc char[typeIdStr.Length + 10];
+        decoded.TryFormat(formattedTypeId, out var charsWritten, "", null).Should().BeTrue();
+
+        formattedTypeId[..charsWritten].ToString().Should().Be(typeIdStr);
+    }
+    
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.ValidIds))]
+    public void Decoded_TryFormatUtf8(string typeIdStr, Guid expectedGuid, string expectedType)
+    {
+        var decoded = TypeId.FromUuidV7(expectedType, expectedGuid);
+
+        Span<byte> formattedTypeId = stackalloc byte[typeIdStr.Length + 10];
+        decoded.TryFormat(formattedTypeId, out var bytesWritten, "", null).Should().BeTrue();
+
+        Encoding.UTF8.GetString(formattedTypeId[..bytesWritten]).Should().Be(typeIdStr);
     }
 }
