b4c594e21e01d90687fb7aa896e832bf13e01d37
Mykhailo Matviiv <firenero94@gmail.com>
2024-07-06 10:08:28 +0200

    Remove dependency on UUIDNext

---
+++ src/FastIDs.TypeId/TypeId.Core/Uuid/GuidConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FastIDs.TypeId.Uuid;
+
+// The UUIDv7 implementation is extracted from https://github.com/mareek/UUIDNext to prevent transient dependency.
+// TypeID doesn't require any UUID implementations except UUIDv7.
+internal static class GuidConverter
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Guid CreateGuidFromBigEndianBytes(Span<byte> bytes)
+    {
+        SetVersion(bytes);
+        SetVariant(bytes);
+        return new Guid(bytes, true);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void SetVersion(Span<byte> bytes)
+    {
+        const byte uuidVersion = 7;
+        const int versionByteIndex = 6;
+        //Erase upper 4 bits
+        bytes[versionByteIndex] &= 0b0000_1111;
+        //Set 4 upper bits to version
+        bytes[versionByteIndex] |= uuidVersion << 4;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void SetVariant(Span<byte> bytes)
+    {
+        const int variantByteIndex = 8;
+        //Erase upper 2 bits
+        bytes[variantByteIndex] &= 0b0011_1111;
+        //Set 2 upper bits to variant
+        bytes[variantByteIndex] |= 0b1000_0000;
+    }
+}
---
+++ src/FastIDs.TypeId/TypeId.Core/Uuid/UuidDecoder.cs
@@ -0,0 +1,25 @@
+﻿using System.Buffers.Binary;
+
+namespace FastIDs.TypeId.Uuid;
+
+// The UUIDv7 implementation is extracted from https://github.com/mareek/UUIDNext to prevent transient dependency.
+// TypeID doesn't require any UUID implementations except UUIDv7.
+internal static class UuidDecoder
+{
+    public static (long timestampMs, short sequence) Decode(Guid guid)
+    {
+        Span<byte> bytes = stackalloc byte[16];
+        guid.TryWriteBytes(bytes, bigEndian: true, out _);
+
+        Span<byte> timestampBytes = stackalloc byte[8];
+        bytes[..6].CopyTo(timestampBytes[2..]);
+        var timestampMs = BinaryPrimitives.ReadInt64BigEndian(timestampBytes);
+
+        var sequenceBytes = bytes[6..8];
+        //remove version information
+        sequenceBytes[0] &= 0b0000_1111;
+        var sequence = BinaryPrimitives.ReadInt16BigEndian(sequenceBytes);
+
+        return (timestampMs, sequence);
+    }
+}
