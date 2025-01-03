4767637ed7092c67e9a45e6fbd5e1db63be2aab2
Mykhailo Matviiv <firenero94@gmail.com>
2025-01-03 10:50:01 +0100

    TypeId IComparable tests

---
+++ src/FastIDs.TypeId/TypeId.Tests/TypeIdTests/ComparisonTests.cs
@@ -0,0 +1,26 @@
+﻿using FastIDs.TypeId;
+using FluentAssertions;
+
+namespace TypeIdTests.TypeIdTests;
+
+[TestFixture]
+public class ComparisonTests
+{
+    [Test]
+    public void TypeId_DifferentPrefix_PrefixesComparedLexicographically()
+    {
+        var first = TypeId.New("bbb").Encode();
+        var second = TypeId.New("aaa").Encode();
+
+        first.Should().BeGreaterThan(second);
+    }
+    
+    [Test]
+    public void TypeId_SamePrefix_OlderIdIsLessThanNewer()
+    {
+        var first = TypeId.New("aaa").Encode();
+        var second = TypeId.New("aaa").Encode();
+
+        first.Should().BeLessThan(second);
+    }
+}
