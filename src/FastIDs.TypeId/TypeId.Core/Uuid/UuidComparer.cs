a680d4d3445e81db110e70328245faa0059c5b5b
Mykhailo Matviiv <firenero94@gmail.com>
2025-01-03 10:50:33 +0100

    Uuidv7 comparer

---
+++ src/FastIDs.TypeId/TypeId.Core/Uuid/UuidComparer.cs
@@ -0,0 +1,26 @@
+﻿namespace FastIDs.TypeId.Uuid;
+
+/// <summary>
+/// Compares two UUIDv7 implementations in <see cref="Guid"/>
+/// </summary>
+internal class UuidComparer : IComparer<Guid>
+{
+    public int Compare(Guid x, Guid y)
+    {
+        const int bytesCount = 16;
+        Span<byte> xBytes = stackalloc byte[bytesCount];
+        x.TryWriteBytes(xBytes, bigEndian: true, out _);
+
+        Span<byte> yBytes = stackalloc byte[bytesCount];
+        y.TryWriteBytes(yBytes, bigEndian: true, out _);
+
+        for (var i = 0; i < bytesCount; i++)
+        {
+            var compareResult = xBytes[i].CompareTo(yBytes[i]);
+            if (compareResult != 0)
+                return compareResult;
+        }
+
+        return 0;
+    }
+}
