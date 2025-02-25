04479759eb60d173e0b5b0c4bba3d61a8bb72fee
Mykhailo Matviiv <firenero94@gmail.com>
2025-02-25 20:43:19 +0100

    Add static comparers to TypeIdDecoded

---
+++ src/FastIDs.TypeId/TypeId.Core/TypeIdDecodedComparers.cs
@@ -0,0 +1,31 @@
+﻿using FastIDs.TypeId.Uuid;
+
+namespace FastIDs.TypeId;
+
+public class TypeIdDecodedLexComparer : IComparer<TypeIdDecoded>
+{
+    public static TypeIdDecodedLexComparer Instance { get; } = new();
+    
+    public int Compare(TypeIdDecoded x, TypeIdDecoded y)
+    {
+        var typeComparison = string.CompareOrdinal(x.Type, y.Type);
+        if (typeComparison != 0) 
+            return typeComparison;
+
+        return UuidComparer.Instance.Compare(x.Id, y.Id);
+    }
+}
+
+public class TypeIdDecodedTimestampComparer : IComparer<TypeIdDecoded>
+{
+    public static TypeIdDecodedTimestampComparer Instance { get; } = new();
+    
+    public int Compare(TypeIdDecoded x, TypeIdDecoded y)
+    {
+        var idComparison = UuidComparer.Instance.Compare(x.Id, y.Id);
+        if (idComparison != 0)
+            return idComparison;
+
+        return string.CompareOrdinal(x.Type, y.Type);
+    }
+}
--- src/FastIDs.TypeId/TypeId.Tests/TypeIdTests/ComparisonTests.cs
+++ src/FastIDs.TypeId/TypeId.Tests/TypeIdTests/ComparisonTests.cs
@@ -1,4 +1,5 @@
-﻿using FastIDs.TypeId;
+﻿using System;
+using FastIDs.TypeId;
 using FastIDs.TypeId.Uuid;
 using FluentAssertions;
 
@@ -53,4 +54,48 @@ public class ComparisonTests
         var result = UuidComparer.Instance.Compare(first.Id, first.Id);
         result.Should().Be(0);
     }
+
+    [TestCase("bbb", "aaa", 1)]
+    [TestCase("aaa", "bbb", -1)]
+    public void TypeIdDecodedLexComparer_DifferentPrefix_TypesComparedLexicographically(string firstPrefix, string secondPrefix, int expected)
+    {
+        var first = TypeIdDecoded.New(firstPrefix);
+        var second = TypeIdDecoded.New(secondPrefix);
+
+        var result = TypeIdDecoded.Comparers.Lex.Compare(first, second);
+        result.Should().Be(expected);
+    }
+    
+    [Test]
+    public void TypeIdDecodedLexComparer_SamePrefix_OlderIdIsLessThanNewer()
+    {
+        var first = TypeIdDecoded.New("aaa");
+        var second = TypeIdDecoded.New("aaa");
+
+        var result = TypeIdDecoded.Comparers.Lex.Compare(first, second);
+        result.Should().BeLessThan(0);
+    }
+    
+    [Test]
+    public void TypeIdDecodedTimestampComparer_DifferentTimestamps_OlderIdIsLessThanNewer()
+    {
+        var first = TypeIdDecoded.New("aaa");
+        var second = TypeIdDecoded.New("aaa");
+
+        var result = TypeIdDecoded.Comparers.Timestamp.Compare(first, second);
+        result.Should().BeLessThan(0);
+    }
+    
+    [TestCase("aaa", "bbb", -1)]
+    [TestCase("bbb", "aaa", 1)]
+    [TestCase("aaa", "aaa", 0)]
+    public void TypeIdDecodedTimestampComparer_SameUuidv7_TypeIsComparedLexicographically(string firstPrefix, string secondPrefix, int expected)
+    {
+        var first = TypeIdDecoded.FromUuidV7(firstPrefix, Guid.Parse("01953e9c-1f53-771b-a158-cb2c3c59bed4"));
+        var second = TypeIdDecoded.FromUuidV7(secondPrefix, Guid.Parse("01953e9c-1f53-771b-a158-cb2c3c59bed4"));
+
+        var result = TypeIdDecoded.Comparers.Timestamp.Compare(first, second);
+        result.Should().Be(expected);
+    }
+    
 }
